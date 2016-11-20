f9aa4f54819c8926ff652ad1d60dfc66b8cfcdeb
Stefan Ziegler <edi.gonzales@gmail.com>
2016-11-20 18:01:47 +0100

    Initialimport

---
+++ src/avdpool/src/main/java/org/catais/avdpool/AvdpoolApplication.java
@@ -0,0 +1,12 @@
+package org.catais.avdpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvdpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvdpoolApplication.class, args);
+	}
+}
---
+++ src/avdpool/src/main/java/org/catais/avdpool/domain/Community.java
@@ -0,0 +1,10 @@
+package org.catais.avdpool.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="community")
+public class Community {
+
+}
